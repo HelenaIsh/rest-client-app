660d50e5af48f99a03004d07b5211755ec62f95b
HelenaIsh <32093844+HelenaIsh@users.noreply.github.com>
2025-04-02 14:03:32 +0500

    fix: add validation for endpoint url
    Co-authored-by: mayskii <94030074+mayskii@users.noreply.github.com>

--- src/app/client/components/EndpointInput.tsx
+++ src/app/client/components/EndpointInput.tsx
@@ -5,13 +5,24 @@ export default function EndpointInput({
   endpointUrl?: string;
   setEndpointUrl: (endpointUrl: string) => void;
 }) {
+
+  const isValid = (() => {
+    try {
+      return !!endpointUrl && new URL(endpointUrl);
+    } catch {
+      return false;
+    }
+  })();
   return (
     <input
-      className="flex-1 p-2 border border-gray-300 rounded-md"
+      className={`flex-1 p-2 border rounded-md ${
+        isValid ? 'border-gray-300' : 'border-red-500'
+      }`}
       type="text"
       value={endpointUrl}
       onChange={(e) => setEndpointUrl(e.target.value)}
       placeholder={'Enter Endpoint URL'}
+      title={isValid ? '' : 'Invalid URL'}
     />
   );
 }
