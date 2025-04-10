2186106707ac7dae380d93caf26cffd11e50a909
HelenaIsh <elena-ishmatova@mail.ru>
2025-04-10 15:39:34 +0200

    chore: add aliases to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -7,5 +8,11 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     include: ['**/*.test.{ts,tsx}'],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/app/[locale]/client/components'),
+    },
   },
 });
