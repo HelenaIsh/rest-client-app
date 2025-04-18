dbc6e2c9cfd8e7bde0c9460095f4a397fceaeaa2
HelenaIsh <32093844+HelenaIsh@users.noreply.github.com>
2025-04-18 22:11:40 +0200

    Auth (#34)
    * feat: implement basic auth
    
    * fix: change user getting
    
    * fix: update routes only on mount
    
    * feat: add unified Loading component for all fallbacks
    
    * feat: add errors messages
    
    * fix: fix general errors
    
    * fix: return previous naming
    
    * fix: add all special symbols to password validation
    
    * fix: add internationalization for all messages
    
    * fix: delete console.error

--- src/app/[locale]/(auth)/signup/page.tsx
+++ src/app/[locale]/(auth)/signup/page.tsx
@@ -1,12 +1,148 @@
+'use client';
+
 import { useTranslations } from 'next-intl';
+import {
+  useAuthState,
+  useCreateUserWithEmailAndPassword,
+} from 'react-firebase-hooks/auth';
+import { auth } from '@/app/firebase/config';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import Loading from '@/components/Loading';
+import Toast from '@/components/Toast';
 
 const SignUpPage: React.FC = () => {
   const t = useTranslations('SignUpPage');
+  const [createUserWithEmailAndPassword, , , error] =
+    useCreateUserWithEmailAndPassword(auth);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({ email: '', password: '' });
+  const [user, loading] = useAuthState(auth);
+  const router = useRouter();
+  const [toastMessage, setToastMessage] = useState<{
+    message: string;
+    type: 'success' | 'error';
+  } | null>(null);
+
+  useEffect(() => {
+    if (!loading && user) {
+      router.push('/');
+    }
+  }, [user, loading, router]);
+
+  useEffect(() => {
+    if (error) {
+      let errorMessage = t('errors.unknown');
+      switch (error.code) {
+        case 'auth/email-already-in-use':
+          errorMessage = t('errors.emailInUse');
+          break;
+        case 'auth/invalid-email':
+          errorMessage = t('errors.invalidEmail');
+          break;
+        case 'auth/operation-not-allowed':
+          errorMessage = t('errors.operationNotAllowed');
+          break;
+        case 'auth/weak-password':
+          errorMessage = t('errors.weakPassword');
+          break;
+      }
+      setToastMessage({
+        message: errorMessage,
+        type: 'error',
+      });
+    }
+  }, [error, t]);
+
+  const handleSignUp = async () => {
+    try {
+      const res = await createUserWithEmailAndPassword(email, password);
+      if (res) {
+        setEmail('');
+        setPassword('');
+        setErrors({ email: '', password: '' });
+        router.push('/');
+      }
+    } catch {}
+  };
+
+  const validateEmail = (email: string) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  };
+
+  const validatePassword = (password: string) => {
+    return (
+      password.length >= 8 &&
+      /[\p{L}]/u.test(password) &&
+      /[0-9]/.test(password) &&
+      /[ !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/.test(password)
+    );
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const newErrors = {
+      email: validateEmail(email) ? '' : t('errors.invalidEmail'),
+      password: validatePassword(password)
+        ? ''
+        : t('errors.passwordRequirements'),
+    };
+    setErrors(newErrors);
+    if (!newErrors.email && !newErrors.password) {
+      await handleSignUp();
+    }
+  };
+
+  if (loading) {
+    return <Loading className="h-full" />;
+  }
 
   return (
-    <div>
-      <h1>{t('title')}</h1>
-      <p>{t('description')}</p>
+    <div className="max-w-7xl mx-auto p-4 bg-white text-gray-500 rounded-2xl shadow-lg flex flex-col relative">
+      {toastMessage && (
+        <Toast
+          message={toastMessage.message}
+          type={toastMessage.type}
+          onClose={() => setToastMessage(null)}
+        />
+      )}
+      <form onSubmit={handleSubmit} className="max-w-sm mx-auto p-4 space-y-4">
+        <h2 className="text-xl font-semibold text-center">{t('mode')}</h2>
+
+        <div>
+          <input
+            type="email"
+            placeholder={t('form.emailPlaceholder')}
+            className="w-full border rounded px-3 py-2"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {errors.email && (
+            <p className="text-red-500 text-sm mt-1">{errors.email}</p>
+          )}
+        </div>
+
+        <div>
+          <input
+            type="password"
+            placeholder={t('form.passwordPlaceholder')}
+            className="w-full border rounded px-3 py-2"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {errors.password && (
+            <p className="text-red-500 text-sm mt-1">{errors.password}</p>
+          )}
+        </div>
+
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white rounded py-2 hover:bg-blue-700"
+        >
+          {t('mode')}
+        </button>
+      </form>
     </div>
   );
 };
