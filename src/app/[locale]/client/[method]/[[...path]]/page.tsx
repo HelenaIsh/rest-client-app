c3b496acf3ae4fa7367e268182eb28a6162a67cf
HelenaIsh <elena-ishmatova@mail.ru>
2025-04-20 21:42:09 +0200

    fix: add key to reload RestClient

--- src/app/[locale]/client/[method]/[[...path]]/page.tsx
+++ src/app/[locale]/client/[method]/[[...path]]/page.tsx
@@ -61,6 +61,7 @@ export default function RequestPage() {
         initialUrl={endpointUrl}
         initialBody={requestBody}
         initialHeaders={headers}
+        key={Date.now()}
       />
     </div>
   );
